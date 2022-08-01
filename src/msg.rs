b6294ce14b0fb9053e3aae15eb325e00410012fd
Ethan Frey <ethanfrey@users.noreply.github.com>
2022-08-01 08:02:48 +0200

    Define state

--- src/msg.rs
+++ src/msg.rs
@@ -2,7 +2,7 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct InstantiateMsg { }
+pub struct InstantiateMsg {}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
@@ -24,18 +24,12 @@ pub enum ExecuteMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    GetEvent {
-        name: String,
-    },
+    GetEvent { name: String },
     // FIXME: add pagination
-    ListAllEvents { },
+    ListAllEvents {},
     // FIXME: add pagination
-    ListAttendees {
-        name: String,
-    },
-    ListMyBadges {
-        attendee: String,
-    },
+    ListAttendees { name: String },
+    ListMyBadges { attendee: String },
 }
 
 // We define a custom struct for each query response
--- src/state.rs
+++ src/state.rs
@@ -2,12 +2,25 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw_storage_plus::Map;
+
+pub const EVENTS: Map<&str, EventData> = Map::new("events");
+// (event name, attendee address)
+pub const ATTENDEES: Map<(&str, &Addr), BadgeData> = Map::new("attendees");
+// (attendee address, event name)
+pub const BADGES: Map<(&Addr, &str), BadgeData> = Map::new("badges");
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
+pub struct EventData {
     pub owner: Addr,
+    pub name: String,
+    pub image: String,
+    pub description: String,
+    pub start_time: u64,
+    pub end_time: u64,
 }
 
-pub const STATE: Item<State> = Item::new("state");
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct BadgeData {
+    was_late: bool,
+}
